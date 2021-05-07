8774ecd70d36505ae8aae783e80432bc84831c88
Vitor Falcao <vitorfhc@protonmail.com>
2021-05-07 16:00:22 -0300

    Add employees seeds

---
+++ hasura/seeds/1620412938806_employees.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.employee (cpf, employer_cnpj) VALUES ('528.609.350-70', '48.395.490/0001-07');
+INSERT INTO public.employee (cpf, employer_cnpj) VALUES ('047.308.040-07', '48.395.490/0001-07');
+INSERT INTO public.employee (cpf, employer_cnpj) VALUES ('469.379.250-00', '17.471.124/0001-58');
+INSERT INTO public.employee (cpf, employer_cnpj) VALUES ('290.785.280-95', '17.471.124/0001-58');
