62e1c8ca6ee37f66c53d2d1d45b0dcc940697d48
Vitor Falcao <vitorfhc@protonmail.com>
2021-05-07 12:48:23 -0300

    Add employers seeds

---
+++ hasura/seeds/1620402488662_employers.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.employer (uuid, cnpj, name, password) VALUES ('e5977ac2-3afa-437e-bbfa-381152e159b5', '48.395.490/0001-07', 'Monstros SA', '62ed366c8b381ac429bff3fd7e1899c8');
+INSERT INTO public.employer (uuid, cnpj, name, password) VALUES ('d7845361-043c-46f9-a4a6-0cf1744e36cd', '17.471.124/0001-58', 'MIB', 'b5ec1a88f4e0fee94cb2288dd6e148aa');
